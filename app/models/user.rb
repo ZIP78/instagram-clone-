848d302bb978ed66bb3ceebe547f646866954795
zip78 <paulkim963@gmail.com>
2019-12-17 10:12:56 -0600

    fixed bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     attr_reader :password
     validates :username, :password_digest, :email, :first_name, :last_name, :session_token, presence: true
-    validates :username, :email, :session_token uniqueness: true
+    validates :username, :email, :session_token, uniqueness: true
     validates :password, length: {minimum: 6}, allow_nil: true 
 
 
