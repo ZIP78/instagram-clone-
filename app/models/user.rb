a298f96698941fce7d55a323c0c3ee3c6b815edc
zip78 <paulkim963@gmail.com>
2019-12-17 12:58:54 -0600

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     validates :username, :email, :session_token, uniqueness: true
     validates :password, length: {minimum: 6}, allow_nil: true 
 
-    after_initalize :ensure_session_token
+    after_initialize :ensure_session_token
 
     # has_many :photos
     # has_many :likes
