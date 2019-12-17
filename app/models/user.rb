f1cfff4e619eca48f26d70565f2bb960c614e832
zip78 <paulkim963@gmail.com>
2019-12-17 12:49:53 -0600

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,35 @@ class User < ApplicationRecord
     validates :username, :email, :session_token, uniqueness: true
     validates :password, length: {minimum: 6}, allow_nil: true 
 
+    after_initalize :ensure_session_token
+
+    # has_many :photos
+    # has_many :likes
+    # has_many :comments
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        unless user && user.is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||=  SecureRandom.urlsafe_base64
+    end
+
 
 end
