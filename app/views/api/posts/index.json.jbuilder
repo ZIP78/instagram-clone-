e8115f0ff13a38711ce0374921fe2c4aa02b806f
zip78 <paulkim963@gmail.com>
2020-01-02 12:49:30 -0600

    master key

--- app/views/api/posts/index.json.jbuilder
+++ app/views/api/posts/index.json.jbuilder
@@ -1,3 +1,5 @@
 json.array! @posts do |post|
     json.extract! post, :id, :user_id, :body
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -61,3 +61,4 @@ ActiveRecord::Schema.define(version: 2019_12_31_221152) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
+
