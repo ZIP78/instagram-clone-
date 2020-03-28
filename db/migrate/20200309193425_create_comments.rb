f496f85ac7f609c3cdd5527ac6fe5a1bca9d5510
zip78 <paulkim963@gmail.com>
2020-03-27 19:03:17 -0500

    migration heroku issue

--- db/migrate/20200309193425_create_comments.rb
+++ db/migrate/20200309193425_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
-    create_table :comments do |t|
+    # create_table :comments do |t|
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
