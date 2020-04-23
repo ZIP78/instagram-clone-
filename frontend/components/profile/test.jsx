021cbf2aca24c1863521544d2183e16cc49069e1
zip78 <paulkim963@gmail.com>
2020-04-23 16:21:18 -0500

    handle photo function

--- frontend/components/profile/test.jsx
+++ frontend/components/profile/test.jsx
@@ -11,12 +11,26 @@ class ProfilePic extends React.Component {
   }
 
   // add a submit photo
+  handlePhoto(e) {
+    e.preventDefault();
+    let photoInput = document.getElementById("photo-input");
+    if (photoInput.files[0]) {
+      const formData = new FormData();
+      const uploadFile = photoInput.files[0];
+      formData.append("profilePicture", uploadFile);
+    }
+  }
 
   render() {
     return (
       <div>
         <form>
-          <input type="file" accept="image/png, image/jpeg" />
+          <input
+            type="file"
+            id="photo-input"
+            name="profilePicture"
+            accept="image/png, image/jpeg"
+          />
           <button>test upload</button>
         </form>
       </div>
