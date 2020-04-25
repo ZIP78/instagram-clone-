23c3d2d0b74bd018cd144c0bf7add840c9c26b20
zip78 <paulkim963@gmail.com>
2020-04-24 19:36:40 -0500

    remove likes api view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :api, defaults: {format: :json} do
         resources :users, only: [:create, :index]
         resource :session, only:[:create, :show, :destroy]  
+        
         resources :posts, only: [:create, :show, :index] do
                 resources :likes, only: [:create]
         end
