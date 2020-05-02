dc615cc4204225f0fac8f30d74ae3a950b5e028e
zip78 <paulkim963@gmail.com>
2020-05-01 19:24:35 -0500

    create profile page icon

--- frontend/components/profile_page/profile_page.jsx
+++ frontend/components/profile_page/profile_page.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./profile_page.css";
+
 class ProfilePage extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Profile Page</h1>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <div className="profile_pic_page_container">
+          <div className="profile_page_pic_container">
+            <div className="profile_page_pic_upload"></div>
+          </div>
+          <div className="profile_page_information_container"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ProfilePage
+export default ProfilePage;
